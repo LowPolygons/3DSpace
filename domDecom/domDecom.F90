d9ed6997edbdf8344e91fad7b060a3726abfad13
LowPolygons <tristan2rimmer05@gmail.com>
2024-10-03 11:53:38 +0100

    Problem Resolved1
    Issue one: Cut off was too big for some processor numbers. This has been resolved
    
    Issue Two: In the MPI_SENDRECV, i hadn't swapped around the rank source and locations for the 2nd set of paritcles per loop meaning ti was sending before and after particles in the same direction. This is why it worked for 1,2,4,8 processors where there are at most 2 processes on each

--- domDecom/domDecom.F90
+++ domDecom/domDecom.F90
@@ -52,11 +52,8 @@ program vectors
 
 	lowerBound = [0.0, 0.0, 0.0]
 	upperBound = [1.0, 1.0, 1.0]
-	cutoff = 0.1
+	cutoff = 0.05
 
-	! lowerBound = [-0.10000000149011612, -18.150000001490117 , -14.540000001490116]!
-	! upperBound = [18.150000001490117, 0.10000000149011612, 0.10000000149011612]   !
-	! cutoff = 4.0 
 	
 	totalSum = 0
 	tempVar = 0
@@ -68,6 +65,14 @@ program vectors
 
 	call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)	
 
+
+	!TODO: FIGURE OUT WHY PROCESSOR RANKS OTHER THAN 1, 2, 4, 8 FAIL
+	!NOTES: 
+	!- IT IS NOT IMPROPERLY DISTRIBUTING PARTICLES; OR AT LEAST THEY ARE SUMMING CORRECTLY
+	!- THE NUMBER OUTPUTS ARE COMPELTELY DETERMINISTIC PER NPROCS
+	!- Worth adding a 4th array index slot per particle as a global index, then you can see specifically which pairs are going missing
+	!- Add a logger at specific intervals so you can see the programs flow throughout longer executions
+	!- Doesn't right now work for extreme upper and lower bounds. Investigate this 
 
 	if (rank == 0) then
 		open(11, file="pData.dat", status="old")
@@ -94,27 +99,17 @@ program vectors
 
 		numDomains = determineNoSplits(nprocs)
 
+		cutoff = min( min(cutoff, (upperBound(1)-lowerBound(1))/numDomains(1)*2), &
+			min( (upperBound(2)-lowerBound(2))/numDomains(2)*2, (upperBound(3)-lowerBound(3))/numDomains(3)*2) )
+
 		12 format("Space divided across:", i4, " processors. Number of domains per axis:", i4, ",", i4, "," i4)
 		print 12, nprocs, numDomains(1), numDomains(2), numDomains(3)
 
 		processorRange = getBounds(nprocs, lowerBound, upperBound, numDomains) !determineBounds(nprocs, lowerBound, upperBound) !
-		!processorRange = determineBounds(nprocs, lowerBound, upperBound) !
-		
-		! do counter1 = 1, nprocs
-		! 	print *, processorRange(counter1, 1:6)
-		! end do 
-		!this now works, until adjacent regions work, still only use a power of 2
-		
-		!processorRange = getBounds(nprocs, lowerBound, upperBound, numDomains)
-		! print *, numDomains
-		! do counter1 = 1, 27
-		! 	test = getAdjacents(counter1-1, numDomains)
-		! end do
-		!the range of each process
+
 		allocate(processDataHolder(size(particlePositions)/3, 3))
 		processDataHolder = upperBound(1)*100 !arbitrary number so we know when its wrong
 	
-		!print *, "finding which particles go where and sending this to the various process"
 		do counter2 = 1, nprocs !currentProcessr
 			processTracker = 0
 			processDataHolder = upperBound(1)*10
@@ -168,9 +163,11 @@ program vectors
 
 	!send to all other processors the num of domains
 	call MPI_BCAST(numDomains, 3, MPI_INTEGER, 0, MPI_COMM_WORLD, ierr)
+	call MPI_BCAST(cutoff, 1, MPI_REAL, 0, MPI_COMM_WORLD, ierr)
 	adjacents = getAdjacents(rank, numDomains)
 	!adjacents = getAdjacantRegions(nprocs, rank)
-	print *, rank, adjacents
+	!print *, rank, adjacents
+
 	particlePairCount = 0
 	do counter2 = 1, arraySize
 		do counter3 = counter2+1, arraySize
@@ -186,7 +183,7 @@ program vectors
 
 	counter2 = 0
 
-	!print *, rank, size(sentProcessData)/3
+	print *, rank, ":", adjacents
 
 	do counter1 = 1, 5, 2
 
@@ -216,15 +213,15 @@ program vectors
 		end do
 
 		numRecieved = 0
-		CALL MPI_Sendrecv(processTracker, 1, MPI_INTEGER, adjacents(counter1), rank*1*counter2, &
-            numRecieved, 1, MPI_INTEGER, adjacents(counter1+1), adjacents(counter1+1)*1*counter2, &
-            MPI_COMM_WORLD, status1, ierr)
+		CALL MPI_Sendrecv(processTracker, 1, MPI_INTEGER, adjacents(counter1), 2, &
+           				     numRecieved, 1, MPI_INTEGER, adjacents(counter1+1), 2, &
+           		 MPI_COMM_WORLD, status1, ierr)
 
 		numRecievedAfter = 0
 
-		CALL MPI_Sendrecv(oppositeCount, 1, MPI_INTEGER, adjacents(counter1), rank*5*counter2, &
-            numRecievedAfter, 1, MPI_INTEGER, adjacents(counter1+1), adjacents(counter1+1)*5*counter2, &
-            MPI_COMM_WORLD, status1, ierr)
+		CALL MPI_Sendrecv(oppositeCount, 1, MPI_INTEGER, adjacents(counter1+1), 3, &
+           			   numRecievedAfter, 1, MPI_INTEGER, adjacents(counter1), 3, &
+            		   MPI_COMM_WORLD, status1, ierr)
 
 		allocate(receivedThese(numRecieved, 3))
 		allocate(receivedTheseAfter(numRecievedAfter, 3))
@@ -241,16 +238,15 @@ program vectors
 			transferTheseAfter(counter3, 1:3) = transferTheseAfter(counter3, 1:3) - currParticle
 		end do 
 
-		!sendreceive the actual particles
+		!sendreceive the particles to the lower and from the higher
 		CALL MPI_Sendrecv(transferThese(1:processTracker, 1:3), processTracker*3, MPI_REAL, adjacents(counter1), 10, &
-          receivedThese(1:numRecieved, 1:3), numRecieved*3, MPI_REAL, adjacents(counter1+1), 10, &
-          MPI_COMM_WORLD, status1, ierr)
+          					receivedThese(1:numRecieved, 1:3), numRecieved*3, MPI_REAL, adjacents(counter1+1), 10, &
+          			MPI_COMM_WORLD, status1, ierr)
 
 		!For the after particles
-		call MPI_Sendrecv(transferTheseAfter(1:oppositeCount, 1:3), oppositeCount*3, MPI_REAL, &
-		& adjacents(counter1), 10, &
-		& receivedTheseAfter(1:numRecievedAfter, 1:3), numRecievedAfter*3, MPI_REAL, adjacents(counter1+1), 10, &
-		& MPI_COMM_WORLD, status1, ierr)
+		call MPI_Sendrecv(transferTheseAfter(1:oppositeCount, 1:3), oppositeCount*3, MPI_REAL,  adjacents(counter1+1), 10, &
+				    receivedTheseAfter(1:numRecievedAfter, 1:3), numRecievedAfter*3, MPI_REAL,  adjacents(counter1), 10, &
+				& MPI_COMM_WORLD, status1, ierr)
 
 		if (counter2 == 1) currParticle = [lowerAndUpper(4),lowerAndUpper(2),lowerAndUpper(3)]
 		if (counter2 == 2) currParticle = [lowerAndUpper(1),lowerAndUpper(5),lowerAndUpper(3)]
@@ -323,15 +319,7 @@ program vectors
 			call MPI_RECV(counter1, 1, MPI_INT, i, 3, MPI_COMM_WORLD, status1, ierr)
 			particlePairCount = particlePairCount + counter1 
 		end do
-		print *, "Total number of unique pairs: ", particlePairCount
-
-
-		!do counter1 = 1, 27
-			! do counter1 = 1, nprocs
-			! 	print *, processorRange(counter1, 1:6)
-			! end do 
-			! print *, ""
-		!end do 
+		print *, "Total number of unique pairs with cutoff ", cutoff, " : ", particlePairCount
 	end if
 
 
@@ -352,181 +340,6 @@ contains
 		!if (vectorMod(temp) /= 0) print *, p1, p2, vectorMod(temp)
 		if (vectorMod(temp) < cutoff) count = 1
 	end function STSinRange
-
-	!its important to note that each process
-	function getAdjacantRegions(nprocs, currRank) result(adjacents)
-		integer, dimension(6) :: adjacents !xL xR, yU yD, zF zB
-		integer, intent(in) :: nprocs, currRank
-		integer :: totalPowerOfTwo, curr, i
-		integer :: numXSlices, numYSlices, numZSlices, usedProcs
-		integer, dimension(3) :: slicesToCurr, duplicateSlices
-
-		totalPowerOfTwo = 0 !2^0 is 1 so if only 1 processor is put in, it does it all
-
-		do i = 1, nprocs !nprocs is fine because one scales linearly and one scales exponentially
-			if (2**i > nprocs) then
-				exit
-			else
-				totalPowerOfTwo = i
-			end if
-		end do 
-
-		numXSlices = 0
-		numYSlices = 0
-		numZSlices = 0
-
-		!determines how many times it has been cut in each direction
-		usedProcs = 2**totalPowerOfTwo - 1 !as its 0 to 7 for eg not 1 to 8
-		do i = 0, usedProcs, 4
-			if (i+4 <= usedProcs) numZSlices = numZSlices + 1
-		end do 
-		usedProcs = usedProcs - numZSlices*4
-
-		do i = 0, usedProcs , 2
-			if (i+2 <= usedProcs) numYSlices = numYSlices + 1
-		end do 
-		usedProcs = usedProcs - numYSlices*2
-		
-		numXSlices = usedProcs
-		!as above
-
-		!now do the same for the curr Process
-		slicesToCurr = 0
-		curr = currRank
-		do i = 0, curr, 4
-			if (i+4 <= curr) slicesToCurr(3) = slicesToCurr(3) + 1
-		end do 
-		curr = curr - slicesToCurr(3)*4
-
-		do i = 0, curr, 2
-			if (i+2 <= curr)slicesToCurr(2) = slicesToCurr(2) + 1
-		end do 
-		curr = curr - slicesToCurr(2)*2
-
-
-		slicesToCurr(1) = curr
-
-		!if (currRank == 6) print *, slicesToCurr
-		!now have the number of slices in in each direction to get to your process
-		!just going to do it line by line initially cus thats the easiest way
-		duplicateSlices = slicesToCurr
-
-		!left
-		duplicateSlices(1) = duplicateSlices(1) - 1
-		if (duplicateSlices(1) < 0) duplicateSlices(1) = numXSlices
-
-		adjacents(1) = (duplicateSlices(1) + duplicateSlices(2)*2 + duplicateSlices(3)*4)
-		duplicateSlices(1) = slicesToCurr(1)
-
-		!right
-		duplicateSlices(1) = duplicateSlices(1) + 1
-		if (duplicateSlices(1) > numXSlices) duplicateSlices(1) = 0
-
-		adjacents(2) = (duplicateSlices(1) + duplicateSlices(2)*2 + duplicateSlices(3)*4)
-		duplicateSlices(1) = slicesToCurr(1)
-
-		!down
-		duplicateSlices(2) = duplicateSlices(2) - 1
-		if (duplicateSlices(2) < 0) duplicateSlices(2) = numYSlices
-
-		adjacents(3) = (duplicateSlices(1) + duplicateSlices(2)*2 + duplicateSlices(3)*4)
-		duplicateSlices(2) = slicesToCurr(2)
-
-		!up
-		duplicateSlices(2) = duplicateSlices(2) + 1
-		if (duplicateSlices(2) > numYSlices) duplicateSlices(2) = 0
-
-		adjacents(4) = (duplicateSlices(1) + duplicateSlices(2)*2 + duplicateSlices(3)*4)
-		duplicateSlices(2) = slicesToCurr(2)
-
-		!backward
-		duplicateSlices(3) = duplicateSlices(3) - 1
-		if (duplicateSlices(3) < 0) duplicateSlices(3) = numZSlices
-
-		adjacents(5) = (duplicateSlices(1) + duplicateSlices(2)*2 + duplicateSlices(3)*4)
-		duplicateSlices(3) = slicesToCurr(3)
-
-		!forward
-		duplicateSlices(3) = duplicateSlices(3) + 1
-		if (duplicateSlices(3) > numZSlices) duplicateSlices(3) = 0
-
-		adjacents(6) = (duplicateSlices(1) + duplicateSlices(2)*2 + duplicateSlices(3)*4)
-		duplicateSlices(3) = slicesToCurr(3)
-
-	end function getAdjacantRegions
-
-	function determineBounds(nprocs, lowerBound, upperBound) result(boundaryRanges)
-		!this will return an array of 6 wide vectors for each processor, outlining what their boundaries shall be
-		!this program will be designed to only use a power of 2 number of processors and it will disregard any others
-		!it will do logical divisions, halving the total X, then Y then Z in a cycle for each power of two the number is
-		integer, intent(in) :: nprocs
-		real, dimension(3), intent(in) :: lowerBound, upperBound
-		real, dimension(3) :: maxBoundRange
-		integer, dimension(3) :: divsPerAxis !by default this will be the difference in lower vs upper bound,
-		!divsPerAxis will say how many times each axis have been cut, for easier looping later
-		integer :: totalPowerOfTwo, i, j, k, numProcessorsUsed, numAssignments
-		real, dimension(nprocs,6) :: boundaryRanges !this will store the ranges of processors, in order of Rank 0 -> max
-
-		totalPowerOfTwo = 0 !2^0 is 1 so if only 1 processor is put in, it does it all
-
-		do i = 1, nprocs !nprocs is fine because one scales linearly and one scales exponentially
-			if (2**i > nprocs) then
-				exit
-			else
-				totalPowerOfTwo = i
-			end if
-		end do 
-
-		do i = 1, nprocs
-			boundaryRanges(i, 1:6) = [0.0,0.0,0.0,0.0,0.0,0.0]
-		end do
-
-		numProcessorsUsed = 2**totalPowerOfTwo !this is how many of the process' will be used. Not a superb long term 
-		!solution but it makes the divisions easy
-
-		!now, it will begin the process of determine each boxes' max bounds range
-		maxBoundRange = upperBound - lowerBound
-		!the number of slices needed is the total powers of two
-
-		divsPerAxis = 1 !ignore if equal to zero !not zero because we want the loop to run at least once and if it doesnt get sliced the loop wont run
-
-		do i = 1, totalPowerOfTwo
-			j = max(1, mod(i, totalPowerOfTwo+1)) !so it repeats and is 1 and totalPowerOfTwo inclusive
-			! tested in lua in lua demo website curr = math.max(1, (curr+1) % (maxCount + 1))
-			!print *, i, j
-			maxBoundRange(j) = maxBoundRange(j) / 2
-			divsPerAxis(j) = divsPerAxis(j) + 1
-		end do 
-
-
-		numAssignments = 0
-		i = 0
-		j = 0
-		k = 0
-
-		!based on the previous calcualted values, it determines what the range of values each process will make use of
-		do while(numAssignments < numProcessorsUsed)
-			i = max(1, mod(i+1, divsPerAxis(1)+1))
-			if (i == 1) j = max(1, mod(j+1, divsPerAxis(2)+1))
-			if (i == 1 .and. j == 1) k = max(1, mod(k+1, divsPerAxis(3)+1))
-			!assigning values
-			numAssignments = numAssignments + 1
-
-
-			boundaryRanges(numAssignments, 1) = maxBoundRange(1)*(i-1)
-			boundaryRanges(numAssignments, 2) = maxBoundRange(2)*(j-1)
-			boundaryRanges(numAssignments, 3) = maxBoundRange(3)*(k-1)
-			boundaryRanges(numAssignments, 4) = maxBoundRange(1)*(i)
-			boundaryRanges(numAssignments, 5) = maxBoundRange(2)*(j)
-			boundaryRanges(numAssignments, 6) = maxBoundRange(3)*(k)
-		end do
-
-		! Use the below code to print the boundaries in a nice format
-		! 100 format("Lower : [", 3f10.7, "], Upper : [" 3f10.7, "]")
-		! do i = 1, nprocs
-		! 	print 100, boundaryRanges(i,1:3),boundaryRanges(i,4:6)
-		! end do
-	end function determineBounds
 
 	function getAdjacents(currProcess, axisSplits) result(adjacents)
 		integer, intent(in) :: currProcess
